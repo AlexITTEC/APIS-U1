dee24cd0bd91d461e6ac9706c6b28f85ad2cd775
AlexITTEC <149985000+AlexITTEC@users.noreply.github.com>
2025-04-08 00:21:24 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,6 +6,10 @@ let serviceAccount;
 if (process.env.FIREBASE_CONFIG_JSON) {
   // 🔐 En producción (Render): usar variable de entorno
   serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
+  
+  // 🛠️ Fix para claves con '\n' literales (Render)
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 } else {
   // 🧪 En desarrollo local: usar archivo físico
   serviceAccount = require("./config/serviceAccountKey.json");
@@ -17,4 +21,3 @@ admin.initializeApp({
 
 const db = admin.firestore();
 module.exports = db;
-
